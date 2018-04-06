a79f1ccc9609e9be3f7ff62f6e2f62fdf2250c2e
Frank Taubert <frank.taubert@uni-jena.de>
2018-04-06 16:41:32 +0200

    Add some more prints

--- main.cs
+++ main.cs
@@ -6,7 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+			Console.WriteLine("############");
             Console.WriteLine("Hello World.");
+			Console.WriteLine("############");
         }
     }
 }
